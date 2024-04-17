259228b623e770be024781baa80bd658b30a814a
Yahya Eddhissa <yahya.eddhissa@proton.me>
2024-04-17 03:09:28 +0100

    Fix api

--- 0x01-challenge/status_server/api/v1/app.py
+++ 0x01-challenge/status_server/api/v1/app.py
@@ -11,10 +11,10 @@ app.register_blueprint(app_views)
 
 @app.errorhandler(404)
 def not_found(error):
-    """ json 404 page """
+    """json 404 page"""
     return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == "__main__":
-    # python -m api.v1.app 
+    # python -m api.v1.app
     app.run(host="0.0.0.0", port=5000)
